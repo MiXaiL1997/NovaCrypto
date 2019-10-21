552fab294799d6f650099a06b3bed6627397e27f
Булкин Михаил <mixan47rus@gmail.com>
2019-10-21 03:09:05 +0300

    Релизованно щифрование метолом скиталы

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,37 @@ namespace NovaCrypto
         public MainWindow()
         {
             InitializeComponent();
+            ScytalEnCryptedText1.IsReadOnlyCaretVisible = true;
+        }
+
+        void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !e.Text.All(IsGood);
+        }
+
+        private void OnPasting(object sender, DataObjectPastingEventArgs e)
+        {
+            var stringData = (string)e.DataObject.GetData(typeof(string));
+            if (stringData == null || !stringData.All(IsGood))
+                e.CancelCommand();
+        }
+
+        bool IsGood(char c)
+        {
+            if (c >= '1' && c <= '9')
+                return true;
+            return false;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ScytalEnCryptedText1.IsReadOnly = true;
+            ScytalEnCryptedText1.IsReadOnlyCaretVisible = false;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ScytalEnCryptedText1.Text = ScytaleCipher.Encrypt(ScytalInputText1.Text, Convert.ToInt32(diameter1.Text));
         }
     }
 }
---
+++ ScytaleCipher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovaCrypto
+{
+    class ScytaleCipher
+    {
+        public static string Encrypt(string text, int d)
+        {
+            var k = text.Length % d;
+            if (k > 0)
+            {
+                //дополняем строку пробелами
+                text += new string(' ', d - k);
+            }
+
+            var column = text.Length / d;
+            var result = "";
+
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    result += text[i + column * j].ToString();
+                }
+            }
+
+            return result;
+        }
+
+        public static string Decrypt(string text, int d)
+        {
+            var column = text.Length / d;
+            var symbols = new char[text.Length];
+            int index = 0;
+            for (int i = 0; i < column; i++)
+            {
+                for (int j = 0; j < d; j++)
+                {
+                    symbols[i + column * j] = text[index];
+                    index++;
+                }
+            }
+
+            return string.Join("", symbols);
+        }
+    }
+}
